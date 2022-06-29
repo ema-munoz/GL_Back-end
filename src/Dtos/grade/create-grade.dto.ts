24eae850024297b348a2dcb69409cab16512e38c
Jonathan Balarezo <jse.balarezo@yavirac.edu.ec>
2022-06-29 12:36:02 -0500

    Service

--- src/Dtos/grade/create-grade.dto.ts
+++ src/Dtos/grade/create-grade.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNumber } from "class-validator";
+import {IsNumber } from "class-validator";
 import { PartialType } from "@nestjs/mapped-types";
 
 export class CreateGradeDto {
--- src/controllers/lab_state.controller.ts
+++ src/controllers/lab_state.controller.ts
@@ -1,4 +1,83 @@
-import { Controller } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	ParseIntPipe,
+	Post,
+	Put,
+	Query,
+} from "@nestjs/common";
+import { LabStateService } from "src/services/lab_state.service";
+import { CreateLabStateDto } from "src/Dtos/lab_state/create-lab_state.dto";
+import { UpdateLabStateDto } from "src/Dtos/lab_state/update-lab_state.dto";
 
-@Controller("lab-state")
-export class LabStateController {}
+@Controller("grade")
+export class GradeController {
+	constructor(private labStateService: LabStateService) {}
+	@Get("")
+	@HttpCode(HttpStatus.OK)
+	index(@Query() params: any) {
+		const response = this.labStateService.getAll();
+
+		return {
+			data: response,
+			message: `index`,
+		};
+	}
+
+	@Get("filter")
+	@HttpCode(HttpStatus.OK)
+	filter(@Query("search") search: string) {
+		return {
+			data: search,
+			message: `filter`,
+		};
+	}
+
+	@Get(":id")
+	@HttpCode(HttpStatus.OK)
+	show(@Param("id", ParseIntPipe) id: number) {
+		const response = this.labStateService.getOne(id);
+		return {
+			data: response,
+			message: `show`,
+		};
+	}
+
+	@Post("")
+	@HttpCode(HttpStatus.CREATED)
+	store(@Body() payload: CreateLabStateDto) {
+		const response = this.labStateService.create(payload);
+		return {
+			data: response,
+			message: `created`,
+		};
+	}
+
+	@Put(":id")
+	@HttpCode(HttpStatus.CREATED)
+	update(
+		@Param("id", ParseIntPipe) id: number,
+		@Body() payload: UpdateLabStateDto
+	) {
+		const response = this.labStateService.update(id, payload);
+		return {
+			data: response,
+			message: `update`,
+		};
+	}
+
+	@Delete(":id")
+	@HttpCode(HttpStatus.CREATED)
+	destroy(@Param("id", ParseIntPipe) id: number) {
+		const response = this.labStateService.delete(id);
+		return {
+			data: response,
+			message: `deleted`,
+		};
+	}
+}
