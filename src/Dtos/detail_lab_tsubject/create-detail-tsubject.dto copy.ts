7b49a940f946431d2aff62ba29077ed48c9a481c
Christopher Vasco <cfe.vasco@yavirac.edu.ec>
2022-06-29 13:10:26 -0500

    cambio en dto

--- src/Dtos/detail_lab_tsubject/create-detail-tsubject.dto copy.ts
+++ src/Dtos/detail_lab_tsubject/create-detail-tsubject.dto copy.ts
@@ -1,7 +1,7 @@
-import { IsDate, } from "class-validator";
+import { IsDateString, } from "class-validator";
 
 export class CreateDetailLabTsubjectDto{
 
-    @IsDate()
+    @IsDateString()
     readonly date:Date
 }
