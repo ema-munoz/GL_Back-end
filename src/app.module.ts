067fa743d70ecc9d711379a5bd40dbf80c868150
ema-munoz <ema.munoz@yavirac.edu.ec>
2022-07-04 13:20:24 -0500

    App Module 😉

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,28 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { CareerController } from "./careers/controllers/career.controller";
+import { CareerService } from "./careers/services/career.service";
+import { DetailLabTsubjectController } from "./detail_lab_tsubject/detail-lab-tsubject.controller";
+import { DetailLabTsubjectService } from "./detail_lab_tsubject/detail-lab-tsubject.service";
+import { DetailTeacherSubjectController } from "./detail_teachers_subjects/controllers/detail_teacher_subject.controller";
+import { DetailTeacherSubjectService } from "./detail_teachers_subjects/services/detail_teacher_subject.service";
+import { GradeController } from "./grade/grade.controller";
+import { GradeService } from "./grade/grade.service";
 import { LaboratoryController } from "./laboratories/laboratory.controller";
 import { LaboratoryService } from "./laboratories/laboratory.service";
+import { LabStateController } from "./lab_state_subject/lab_state.controller";
+import { LabStateService } from "./lab_state_subject/lab_state.service";
+import { ParallelController } from "./parallel/parallel.controller";
+import { ParallelService } from "./parallel/parallel.service";
+import { SemesterController } from "./semester/semester.controller";
+import { SemesterService } from "./semester/semester.service";
+import { ShiftController } from "./shifts/shift.controller";
+import { ShiftService } from "./shifts/shift.service";
 import { SubjectController } from "./subjects/subject.controller";
 import { SubjectService } from "./subjects/subject.service";
 import { TeacherController } from "./teachers/teacher.controller";
 import { TeacherService } from "./teachers/teacher.service";
-import { ShiftController } from "./shifts/shift.controller";
-import { ShiftService } from "./shifts/shift.service";
-import { SemesterController } from "./semester/semester.controller";
-import { SemesterService } from "./semester/semester.service";
-import { DetailLabTsubjectController } from "./detail_lab_tsubject/detail-lab-tsubject.controller";
-import { DetailLabTsubjectService } from "./detail_lab_tsubject/detail-lab-tsubject.service";
-import { GradeController } from "./grade/grade.controller";
-import { LabStateController } from "./lab_state_subject/lab_state.controller";
-import { ParallelController } from "./parallel/parallel.controller";
-import { GradeService } from "./grade/grade.service";
-import { LabStateService } from "./lab_state_subject/lab_state.service";
-import { ParallelService } from "./parallel/parallel.service";
-import { CareerController } from "./careers/controllers/career.controller";
-import { CareerService } from "./careers/services/career.service";
-import { DetailTeacherSubjectController } from "./detail_teachers_subjects/controllers/detail_teacher_subject.controller";
-import { DetailTeacherSubjectService } from "./detail_teachers_subjects/services/detail_teacher_subject.service";
-import { CareersModule } from "./careers/careers.module";
-import { DetailTeachersSubjectsModule } from "./detail_teachers_subjects/detail_teachers_subjects.module";
 
 @Module({
 	imports: [
@@ -39,8 +37,6 @@ import { DetailTeachersSubjectsModule } from "./detail_teachers_subjects/detail_
 		// 	entities: [],
 		// 	synchronize: true,
 		// }),
-		CareersModule,
-		DetailTeachersSubjectsModule,
 	],
 
 	controllers: [
--- src/semester/Dtos/create-semester.dto.ts
+++ src/semester/Dtos/create-semester.dto.ts
@@ -1,7 +1,7 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateSemesterDto {
-  @IsNotEmpty() //El campo no este vacio
+	@IsNotEmpty() //El campo no este vacio
 	@IsString() //El campo de texto
 	readonly name: string;
 }
