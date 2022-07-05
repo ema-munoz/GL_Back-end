0c5ae0347bfc147985fa27aa19425a82ecb464ba
Christopher Vasco <cfe.vasco@yavirac.edu.ec>
2022-07-05 11:53:52 -0500

    Cambios menores en importaciones del app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,61 +1,38 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { DetailLabTsubjectController } from "./detail_lab_tsubject/detail-lab-tsubject.controller";
-import { DetailLabTsubjectService } from "./detail_lab_tsubject/detail-lab-tsubject.service";
-import { GradeController } from "./grade/grade.controller";
-import { GradeService } from "./grade/grade.service";
-import { LaboratoryController } from "./laboratories/laboratory.controller";
-import { LaboratoryService } from "./laboratories/laboratory.service";
-import { LabStateController } from "./lab_state_subject/lab_state.controller";
-import { LabStateService } from "./lab_state_subject/lab_state.service";
-import { ParallelController } from "./parallel/parallel.controller";
-import { ParallelService } from "./parallel/parallel.service";
-import { SemesterController } from "./semester/semester.controller";
-import { SemesterService } from "./semester/semester.service";
-import { ShiftController } from "./shifts/shift.controller";
-import { ShiftService } from "./shifts/shift.service";
-import { SubjectController } from "./subjects/subject.controller";
-import { SubjectService } from "./subjects/subject.service";
-import { TeacherController } from "./teachers/teacher.controller";
-import { TeacherService } from "./teachers/teacher.service";
 import { CareersModule } from "./careers/careers.module";
 import { DetailTeachersSubjectsModule } from "./detail_teachers_subjects/detail_teachers_subjects.module";
-//import { CareersModule } from "./careers/careers.module";
-//import { DetailTeachersSubjectsModule } from "./detail_teachers_subjects/detail_teachers_subjects.module";
-//import { LabotoryModule } from "./laboratories/laboratory.module";
+import { LabotoryModule } from "./laboratories/laboratory.module";
+import { SubjectModule } from "./subjects/subject.module";
+import { DetailLabTsubjectModule } from "./detail_lab_tsubject/detail_lab_tsubject.module";
+import { TeacherModule } from "./teachers/teacher.module";
+import { ShiftModule } from "./shifts/shift.module";
+import { SemesterModule } from "./semester/semester.module";
+import { GradeModule } from "./grade/grade.module";
+import { LabStateModule } from "./lab_state_subject/lab_state.module";
+import { ParallelModule } from "./parallel/parallel.module";
 
 @Module({
 	imports: [
 		CareersModule,
 		DetailTeachersSubjectsModule,
-		/*LabotoryModule */
+		LabotoryModule,
+		SubjectModule,
+		DetailLabTsubjectModule,
+		TeacherModule,
+		ShiftModule,
+		SemesterModule,
+		GradeModule,
+		LabStateModule,
+		ParallelModule,
 	],
 
 	controllers: [
 		AppController,
-		LaboratoryController,
-		TeacherController,
-		ShiftController,
-		SemesterController,
-		GradeController,
-		LabStateController,
-		ParallelController,
-		SubjectController,
-		DetailLabTsubjectController,
 	],
 	providers: [
 		AppService,
-		LaboratoryService,
-		TeacherService,
-		ShiftService,
-		SemesterService,
-		GradeService,
-		LabStateService,
-		ParallelService,
-		SubjectService,
-		DetailLabTsubjectService,
 	],
 })
 export class AppModule {}
